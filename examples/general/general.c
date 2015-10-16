05d5b71ccb8050be6a4a048ef43286a9a340839f
Sean Marshallsay <srm.1708@gmail.com>
2015-10-16 16:58:20 +0100

    Slightly more thorough testing.

--- examples/general/general.c
+++ examples/general/general.c
@@ -1,7 +1,6 @@
 #include "general.h"
 
 int main() {
-    Person pete = Person_create((int8_t)18, 117.6, 69.2);
-    Eye blue = Blue;
-    Person_describe(pete, blue);
+    Person pete = Person_create((int8_t)18, Blue, 117.6, 69.2);
+    Person_describe(pete);
 }
--- examples/general/general.rs
+++ examples/general/general.rs
@@ -1,29 +1,33 @@
 #![feature(plugin)]
 #![plugin(cheddar)]
 
+#![allow(non_snake_case)]
+
 cheddar! {"general.h"
     enum Eye {
         Blue,
         Green,
-        Red,
+        Red
     }
 
     struct Person {
         age: i8,
+        eyes: Eye,
         weight: f32,
         height: f32,
     }
 
-    fn Person_create(age: i8, weight_lbs: f32, height_ins: f32) -> Person {
+    fn Person_create(age: i8, eyes: Eye, weight_lbs: f32, height_ins: f32) -> Person {
         Person {
             age: age,
+            eyes: eyes,
             weight: weight_lbs * 0.45,
             height: height_ins * 0.0254,
         }
     }
 
-    fn Person_describe(person: Person, eyes: Eye) {
-        let eyes = match eyes {
+    fn Person_describe(person: Person) {
+        let eyes = match person.eyes {
             Eye::Blue => "blue",
             Eye::Green => "green",
             Eye::Red => "red",
